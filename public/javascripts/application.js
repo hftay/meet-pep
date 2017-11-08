5ea57134c2d4c7be4e10d4f3852cacd59fb5e64f
hsingfu <hsingfu.t@gmail.com>
2017-11-08 23:22:16 +1100

    added google maps api

---
+++ public/javascripts/application.js
@@ -0,0 +1,37 @@
+
+var placeSearch, autocomplete;
+
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')));
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  var lat = place.geometry.location.lat()
+  var lng = place.geometry.location.lng()
+  console.log(place)
+  console.log(place.geometry.location.lat())
+  console.log(place.geometry.location.lng())
+
+}
+
+
+function createMap(){
+  // create a new map
+  var myLatLng = {lat: -37.8, lng: 144.9};
+  var mapOptions = {
+      center: myLatLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.querySelector("#maptwo"), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP 
+  });
+}
